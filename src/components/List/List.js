4e773379551f55fc9172096f6724c14eafeda225
Andysilva98 <andersonsilva.nascimento98@gmail.com>
2022-06-04 18:17:39 -0300

    criação da listagem de usuários

---
+++ src/components/List/List.js
@@ -0,0 +1,29 @@
+import styles from "./List.module.css";
+import { useState, useEffect } from "react";
+
+export default function List() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://www.mocky.io/v2/5d531c4f2e0000620081ddce") //fetch busca os dados de uma api ou documento externo
+      .then((resp) => resp.json()) // cria a função que passa um parametro que recebe os dados, a função transforma os dados JSON
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  return (
+    <>
+      {users.map((user) => (
+        <div className={styles.container} key={user.id}>
+          <img className={styles.img} src={user.img} />
+          <div className={styles.text}>
+            <div>{user.name}</div>
+            <div>{`ID: ${user.id} - Username: ${user.username}`} </div>
+          </div>
+          <button className={styles.button}>Pagar</button>
+        </div>
+      ))}
+    </>
+  );
+}
---
+++ src/components/User/User.js
@@ -0,0 +1,8 @@
+export default function User() {
+  return (
+    <div>
+      <h1>titulo</h1>
+      <p>texto exemplo</p>
+    </div>
+  );
+}
